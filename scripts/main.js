16c77492fdd1e81f9c20b7805b838e0411963ca3
brooklvk <brooklvk@byui.edu>
2023-03-28 20:29:01 -0600

    tried to add highlight selected in nav

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,10 @@ const icon = document.querySelector(".menu");
 
 icon.addEventListener("click", () => {nav.classList.toggle("responsive")}, false);
 
-
+// Highlight active selection in nav 
+const respons = document.querySelector(".respons-a");
+respons.addEventListener("click", () => {respons.classList.toggle("active"), false})
+//doesn't work 
 
 
 // Current year in footer 
