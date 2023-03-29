ff3ba90fc837e8d7f10887236c9c135c43debed5
brooklvk <brooklvk@byui.edu>
2023-03-28 19:26:27 -0600

    edited css and images

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+
+// Hamburger menu in nav 
+const nav = document.querySelector("nav");
+const icon = document.querySelector(".menu");
+
+icon.addEventListener("click", () => {nav.classList.toggle("responsive")}, false);
+
+
+
+
+// Current year in footer 
+const year = new Date().getFullYear();
+document.querySelector("#currentYear").textContent = year;
+
+// Last Modified in footer 
+document.querySelector("#lastModified").innerHTML = document.lastModified;
