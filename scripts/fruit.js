716fd500394876eb8614ea63c945fea8cf2b82a6
brooklvk <brooklvk@byui.edu>
2023-03-31 20:58:55 -0600

    entering order form

--- scripts/fruit.js
+++ scripts/fruit.js
@@ -1,3 +1,7 @@
 
 const url = "https://brotherblazzard.github.io/canvas-content/fruit.json";
 
+//use a JavaScript event handler to produce a formatted output area on this same page showing:
+//the input values of the order (7 inputs = first name, email, phone, three selected fruits, and special instructions), the order date, and
+//the total amount of carbohydrates, protein, fat, sugar, and calories based upon the three fruit choices selected on the form. The data source provides that information per fruit.
+
