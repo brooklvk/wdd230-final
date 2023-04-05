f0a0775b506336dfc74ea24d9e2d0f5abd29e2af
brooklvk <brooklvk@byui.edu>
2023-04-05 11:06:02 -0600

    fresh js is working displays all info ex data

--- scripts/fresh.js
+++ scripts/fresh.js
@@ -7,39 +7,74 @@ async function getFruitData() {
 }
 
 function displayFruitsOrdered(data) {
-    //use a JavaScript event handler to produce a formatted output area on this same page showing:
-    //the input values of the order (7 inputs = first name, email, phone, three selected fruits, and special instructions), the order date, and
-    //the total amount of carbohydrates, protein, fat, sugar, and calories based upon the three fruit choices selected on the form. 
-    // //The data source provides that information per fruit.
 
-    // console.log(data);//works 
+    console.log(data);//test 
 
+    // Keep track of number of orders submitted for home page 
+    // let numOfOrders = 0;
+    // localStorage.setItem("numOfOrders", numOfOrders);
+    // localStorage.clear;
+
+    // Select 'summary' div to hold output information 
     const summary = document.querySelector(".summary");
 
-    let fruit1 = document.querySelector("#fruit1").value;
-    //.options[fruit1.selectedIndex].text;
-    // localStorage.setItem("fruit1", fruit1);
+    // Create a new title and intro 
+    const title = document.createElement("h2");
+    title.innerHTML = "Order Summary";
+    summary.appendChild(title);
+
+    const intro = document.createElement("h3");
+    intro.innerHTML = "Here is the order you submitted!";
+    summary.appendChild(intro);
+
+    // Get all input information from form, and date 
+    const now = new Date().toDateString();
+    const fname = document.querySelector("#fname").value;
+    const email = document.querySelector("#email").value;
+    const phone = document.querySelector("#phone").value;
+
+    const fruit1 = document.querySelector("#fruit1").value;
+    const fruit2 = document.querySelector("#fruit2").value;
+    const fruit3 = document.querySelector("#fruit3").value;
+
+    const instructions = document.querySelector("#instructions").value;
+
+    // Display all information in HTML 
+    const nowDisplay = document.createElement("p");
+    nowDisplay.innerHTML = `${now}`;
+    summary.appendChild(nowDisplay);
+
+    const fnamep = document.createElement("p");
+    fnamep.innerHTML = `First Name: ${fname}`;
+    summary.appendChild(fnamep);
+
+    const emailp = document.createElement("p");
+    emailp.innerHTML = `Email: ${email}`;
+    summary.appendChild(emailp);
+
+    const phonep = document.createElement("p");
+    phonep.innerHTML = `Phone: ${phone}`;
+    summary.appendChild(phonep);
 
     const fruit1p = document.createElement("p");
-    fruit1p.innerHTML = `${fruit1} has ${""}`;
+    fruit1p.innerHTML = `1st Fruit: ${fruit1[0].toUpperCase() + fruit1.substring(1).toLowerCase()}`;
     summary.appendChild(fruit1p);
 
-    let fruit2 = document.querySelector("#fruit2").value;
-    // localStorage.setItem("fruit2", fruit2);
-
     const fruit2p = document.createElement("p");
-    fruit2p.innerHTML = `${fruit2} has ${""}`;
+    fruit2p.innerHTML = `2nd Fruit: ${fruit2[0].toUpperCase() + fruit2.substring(1).toLowerCase()}`;
     summary.appendChild(fruit2p);
 
-    let fruit3 = document.querySelector("#fruit3").value;
-    // localStorage.setItem("fruit3", fruit3);
-
     const fruit3p = document.createElement("p");
-    fruit3p.innerHTML = `${fruit3} has ${""}`;
+    fruit3p.innerHTML = `3rd Fruit: ${fruit3[0].toUpperCase() + fruit3.substring(1).toLowerCase()}`;
     summary.appendChild(fruit3p);
 
-    // const title = document.createElement("h2").innerHTML = "Order Summary";
-    // summary.appendChild(title);
+    const instructionsp = document.createElement("p");
+    instructionsp.innerHTML = `Special Instructions: ${instructions}`;
+    summary.appendChild(instructionsp);
+
+    // Display fruits ordered and data 
+    // Show total amount carbs, protein, fat, sugar, calories in three fruit choices 
+
 
     
 
@@ -48,9 +83,6 @@ function displayFruitsOrdered(data) {
     document.querySelector("form").style.display = "none";
 
     document.querySelector(".order-form h2").style.display = "none";
-
-    
-    // localStorage.clear;
     
 }
 
