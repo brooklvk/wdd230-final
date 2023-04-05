53e9e0c6993f413b11570341b19baa861df437f6
brooklvk <brooklvk@byui.edu>
2023-04-05 14:22:10 -0600

    edited

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,6 @@ const icon = document.querySelector(".menu");
 
 icon.addEventListener("click", () => {nav.classList.toggle("responsive")}, false);
 
-
-// Highlight active selection in nav 
-const respons = document.querySelector(".respons-a");
-respons.addEventListener("click", () => {respons.classList.toggle("active"), false})
-//doesn't work 
-
-
 // Current year in footer 
 const year = new Date().getFullYear();
 document.querySelector("#currentYear").textContent = year;
