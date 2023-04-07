382dcd552217646f20feae06602347f86c74f2e3
brooklvk <brooklvk@byui.edu>
2023-04-07 08:22:24 -0600

    edited submitted

--- scripts/submitted.js
+++ scripts/submitted.js
@@ -3,5 +3,11 @@ const submitted = document.querySelector(".submitted");
 const orders = localStorage.getItem("numOfOrders");
 const p = document.createElement("p");
 
-p.innerHTML = `You have submitted ${orders} drink order(s) on our Fresh page!`;
+if (orders == null) {
+    p.innerHTML = `You have submitted 0 drink order(s) on our Fresh page!`;
+}
+else if (orders != null) {
+    p.innerHTML = `You have submitted ${orders} drink order(s) on our Fresh page!`;
+}
+
 submitted.appendChild(p);
